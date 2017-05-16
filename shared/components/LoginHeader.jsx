040a77538cf9aa3f2e71ad228b87807a97cfe99e
aayush123 <aayush123@gmail.com>
2017-05-16 09:15:05 +0530

    Create LoginHeader.jsx

---
+++ shared/components/LoginHeader.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import {PageHeader} from 'react-bootstrap';
+
+export default class Header extends React.Component{
+  render(){
+    return(
+      <PageHeader id="StoreHeader" style={{textAlign:"center"}}>Login To The Store</PageHeader>
+    )
+  }
+}
