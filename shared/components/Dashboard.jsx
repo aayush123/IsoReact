9193a1d4b4976f59676aac9db68a5ed7f9ba3de5
aayush123 <aayush123@gmail.com>
2017-05-16 09:14:10 +0530

    Create Dashboard.jsx

---
+++ shared/components/Dashboard.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export default class Dashboard extends React.Component{
+  constructor(props){
+    super(props);
+  }
+  render(){
+    return (<div>
+      <h1>This is landing page</h1>
+    </div>)
+  }
+}
