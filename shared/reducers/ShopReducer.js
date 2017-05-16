711c0b9925d05ccf0bb33b188a8fad909cc34153
aayush123 <aayush123@gmail.com>
2017-05-16 09:11:57 +0530

    Create ShopReducer.js

---
+++ shared/reducers/ShopReducer.js
@@ -0,0 +1,9 @@
+import Immutable from 'immutable';
+import { browserHistory } from 'react-router';
+
+export default function ShopReducer(state={'x':10}, action){
+  switch (action.type) {
+    default:
+      return state;
+  }
+}
